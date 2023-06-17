f2cd85218d696b24fce38f7843171d98f6635804
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2023-06-17 17:33:15 +0000

    Configured Mantine UI library

---
+++ src/config/theme.ts
@@ -0,0 +1,7 @@
+import { MantineThemeOverride } from "@mantine/core";
+
+const defaultTheme: MantineThemeOverride = {
+  fontFamily: "Poppins, sans-serif"
+};
+
+export default defaultTheme;
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "@/app.tsx";
 import "./index.scss";
+import { MantineProvider } from "@mantine/core";
+import defaultTheme from "@/config/theme.ts";
 
 // Entry point of the app
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MantineProvider theme={defaultTheme} withGlobalStyles withNormalizeCSS>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MantineProvider>
   </React.StrictMode>
 );
