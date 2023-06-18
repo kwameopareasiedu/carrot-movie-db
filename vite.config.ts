d9d3c4352fefd8765ac442495d9efeba8074666b
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2023-06-18 19:40:27 +0000

    Updated vite config and built files into /doc

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { resolve, join } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/carrot-movie-db",
   plugins: [react()],
+  base: "/carrot-movie-db",
+  build: {
+    outDir: join(__dirname, "docs")
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "src")
